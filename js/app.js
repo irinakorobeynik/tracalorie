04c727598881f4151658d7b481cc04b03ec884e4
k0robeynik <IrinaKorobeynik@coherentsolutions.com>
2025-02-12 14:09:33 +0300

    adding displaying of meals & workouts

--- js/app.js
+++ js/app.js
@@ -13,13 +13,15 @@ class CalorieTracker{
     }
     addMeal(meal) {
         this._meals.push(meal);
-        this._totalCalories += meal.carolies;
+        this._totalCalories += meal.calories;
+        this._displayNewMeal(meal);
         this._render();
     }
     
     addWorkout(workout) {
         this._workouts.push(workout);
-        this._totalCalories -= workout.carolies;
+        this._totalCalories -= workout.calories;
+        this._displayNewWorkout(workout);
         this._render();
     }
     
@@ -35,14 +37,14 @@ class CalorieTracker{
 
     _displayCaloriesConsumed() {
         const caloriesConsumed = document.getElementById('calories-consumed');
-        const consume = this._meals.reduce((total, meal) => total + meal.carolies, 0);
+        const consume = this._meals.reduce((total, meal) => total + meal.calories, 0);
         caloriesConsumed.innerText = consume;
     }
 
     
     _displayCaloriesBurned() {
         const caloriesBurned = document.getElementById('calories-burned');
-        const burn = this._workouts.reduce((total, workout) => total + workout.carolies, 0);
+        const burn = this._workouts.reduce((total, workout) => total + workout.calories, 0);
         caloriesBurned.innerText = burn;
     }
 
@@ -72,6 +74,48 @@ class CalorieTracker{
         progressEl.style.width = `${width}%`;
 
     }
+
+    _displayNewMeal(meal) {
+        const mealsEl = document.getElementById('meal-items');
+        const mealEl = document.createElement('div');
+        mealEl.classList.add('card', 'my-2');
+        mealEl.setAttribute('data-id', meal.id);
+        mealEl.innerHTML =
+        `     <div class="card-body">
+                <div class="d-flex align-items-center justify-content-between">
+                  <h4 class="mx-1">${meal.name}</h4>
+                  <div
+                    class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5">
+                    ${meal.calories}
+                  </div>
+                  <button class="delete btn btn-danger btn-sm mx-2">
+                    <i class="fa-solid fa-xmark"></i>
+                  </button>
+                </div>
+              </div>`;
+        mealsEl.appendChild(mealEl);
+    };
+
+    _displayNewWorkout(workout){
+        const workoutsEl = document.getElementById('workout-items');
+        const workoutEl = document.createElement('div');
+        workoutEl.classList.add('card', 'my-2');
+        workoutEl.setAttribute('data-id', workout.id);
+        workoutEl.innerHTML =
+        `     <div class="card-body">
+                <div class="d-flex align-items-center justify-content-between">
+                  <h4 class="mx-1">${workout.name}</h4>
+                  <div
+                    class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5">
+                    ${workout.calories}
+                  </div>
+                  <button class="delete btn btn-danger btn-sm mx-2">
+                    <i class="fa-solid fa-xmark"></i>
+                  </button>
+                </div>
+              </div>`;
+        workoutsEl.appendChild(workoutEl);
+    };
     
 
     _render() {
@@ -85,18 +129,18 @@ class CalorieTracker{
 }
 
 class Meal{
-    constructor(name, carolies) {
+    constructor(name, calories) {
         this.id = Math.random().toString(16).slice(2);
         this.name = name;
-        this.carolies = carolies;
+        this.calories = calories;
     }
 }
 
 class Workout{
-    constructor(name, carolies) {
+    constructor(name, calories) {
         this.id = Math.random().toString(16).slice(2);
         this.name = name;
-        this.carolies = carolies;
+        this.calories = calories;
     }
 }
 
