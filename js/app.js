c5c06c34ab0115a1b315cbd5a8832640e4ede167
k0robeynik <IrinaKorobeynik@coherentsolutions.com>
2025-02-12 16:03:12 +0300

    adding setting calorie limit

--- js/app.js
+++ js/app.js
@@ -54,6 +54,13 @@ class CalorieTracker{
         this._render();
     }
 
+    setLimit(calorieLimit) {
+        this._calorieLimit = calorieLimit;
+        this._displayCaloriesLimit();
+        this._render();
+        
+    }
+
 
     _displayCaloriesTotal() {
         const totalCaloriesEl = document.getElementById('calories-total');
@@ -184,6 +191,7 @@ class App {
         document.getElementById('filter-meals').addEventListener('keyup', this._filterItem.bind(this, 'meal'));
         document.getElementById('filter-workouts').addEventListener('keyup', this._filterItem.bind(this, 'workout'));
         document.getElementById('reset').addEventListener('click', this._reset.bind(this));
+        document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this));
     }
 
     _newItem(type, e) {
@@ -238,6 +246,21 @@ class App {
         document.getElementById('filter-meals').value = '';
         document.getElementById('filter-workouts').value = '';
 
+    }
+
+    _setLimit(e) {
+        e.preventDefault();
+        const limit = document.getElementById('limit');
+        if (limit.value === '') {
+            alert('Please, add a limit');
+            return;
+        }
+        this._tracker.setLimit(+limit.value);
+        limit.value = '';
+
+        const modalEl = document.getElementById('limit-modal');
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        modal.hide();
     }
         
 }
